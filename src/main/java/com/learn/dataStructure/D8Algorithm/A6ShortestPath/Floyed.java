4f14b553a9551abaaacdd952de38dd377c32201b
yangdongsheng2 <yangdongsheng2@9fbank.com.cn>
2020-12-11 16:57:21 +0800

    弗洛伊德算法 完善路径输出

--- src/main/java/com/learn/dataStructure/D8Algorithm/A6ShortestPath/Floyed.java
+++ src/main/java/com/learn/dataStructure/D8Algorithm/A6ShortestPath/Floyed.java
@@ -3,9 +3,7 @@ package com.learn.dataStructure.D8Algorithm.A6ShortestPath;
 import com.alibaba.fastjson.JSON;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author yds
@@ -38,6 +36,20 @@ public class Floyed {
         Integer numVertexes = graph.getNumVertexes();
         String[] vertexes = graph.getVertexes();
 
+        // 初始化两个顶点之间的前置顶点
+        int[][] prePath = new int[numVertexes][numVertexes];
+        for (int i = 0; i < numVertexes; i++) {
+            for (int j = 0; j < numVertexes; j++) {
+                prePath[i][j] = i;
+            }
+        }
+
+//        for (int i = 0; i < numVertexes; i++) {
+//            System.out.println(Arrays.toString(prePath[i]));
+//        }
+//        System.out.println();
+
+
 //    0. Dis（A，K）+ Dis（K，B）< Dis（A，B）
 //    1.直接从顶点 A 到顶点 B 的弧的权值为顶点 A 到顶点 B 的最短路径；
 //    2.从顶点 A 开始，经过若干个顶点，最终达到顶点 B，期间经过的弧的权值和为顶点 A 到顶点 B 的最短路径。
@@ -58,10 +70,26 @@ public class Floyed {
                     c++;
                     if (weightA + weightB < weightC) {
                         matrix[a][b] = weightA + weightB;
-                        System.out.println("更新结点（" + a + "," + b + "),原长度：" + weightC + "，新长度：" + matrix[a][b]);
+//                        System.out.println("中间结点：" + k +  "，更新结点（" + a + "," + b + "),原长度：" + weightC + "，新长度：" + matrix[a][b]);
+                        prePath[a][b] = prePath[k][b];
+//                        System.out.println("["+a+"][" + b + "] 的前驱置为：" + prePath[a][b]);
                     }
                 }
             }
+        }
+
+
+
+
+//        System.out.println();
+//        for (int i = 0; i < numVertexes; i++) {
+//            System.out.println(Arrays.toString(prePath[i]));
+//        }
+//        System.out.println();
+
+        System.out.println();
+        for (int i = 0; i < numVertexes; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
         }
         System.out.println();
         System.out.println("共执行次数：" + c);
@@ -81,16 +109,35 @@ public class Floyed {
                 nodeResult.setToNodeNo(j);
                 nodeResult.setToNodeDesc(vertexes[j]);
                 nodeResult.setPathDistance(matrix[i][j]);
+
+                int[] pathArray = prePath[i];
+                List<Integer> path = new ArrayList<>();
+//                System.out.println(vertexes[i] + "到" + vertexes[j] + "的距离" + matrix[i][j]);
+//                System.out.println("当前结点：" + j + "-" +vertexes[j] + "的父节点" + pathArray[j]);
+
+                Stack<Integer> pathStack = new Stack<>();
+                int preIndex = j;
+                pathStack.push(j);
+                while (preIndex != pathArray[preIndex]){
+                    preIndex = pathArray[preIndex];
+                    pathStack.push(preIndex);
+                }
+
+                while (!pathStack.isEmpty()){
+                    path.add(pathStack.pop());
+                }
+                nodeResult.setPath(path);
+
                 nodeResultList.add(nodeResult);
+//                System.out.println();
             }
             if (nodeResultList.size() > 0) {
                 floyedResult.put(reInfo, nodeResultList);
             }
         }
 
-        System.out.println(JSON.toJSONString(floyedResult));
+        System.out.println("弗洛伊德结果：" + JSON.toJSONString(floyedResult));
     }
-
 
     @Data
     public static class NodeResult {
@@ -99,6 +146,7 @@ public class Floyed {
         int toNodeNo;
         String toNodeDesc;
         int pathDistance;
+        List<Integer> path;
     }
 
     @Data
@@ -111,28 +159,28 @@ public class Floyed {
     public Graph createGraph() {
         Graph graph = new Graph();
         //无向图
-        String[] vertexes = {"A", "B", "C", "D", "E", "F","G"};
-        int[][] matrix = new int[][]{
-                {10000, 5, 7, 10000, 10000, 10000,2},
-                {5, 10000, 10000, 9, 10000, 10000,3},
-                {7, 10000, 10000, 10000, 8, 10000,10000},
-                {10000, 9, 10000, 10000, 10000, 10,10000},
-                {10000, 10000, 8, 10000, 10000, 5,4},
-                {10000, 10000, 10000, 10, 5, 10000,10000},
-                {2, 3, 10000, 10000, 4, 10000,10000},
-
-        };
+//        String[] vertexes = {"A", "B", "C", "D", "E", "F","G"};
+//        int[][] matrix = new int[][]{
+//                {10000, 5, 7, 10000, 10000, 10000,2},
+//                {5, 10000, 10000, 9, 10000, 10000,3},
+//                {7, 10000, 10000, 10000, 8, 10000,10000},
+//                {10000, 9, 10000, 10000, 10000, 10,10000},
+//                {10000, 10000, 8, 10000, 10000, 5,4},
+//                {10000, 10000, 10000, 10, 5, 10000,10000},
+//                {2, 3, 10000, 10000, 4, 10000,10000},
+//
+//        };
 
         // 有向图
-//        String[] vertexes = {"V0", "V1", "V2", "V3", "V4", "V5"};
-//        int[][] matrix = new int[][]{
-//                {10000, 10000, 10, 10000, 30, 100},
-//                {10000, 10000, 5, 10000, 10000, 10000},
-//                {10000, 10000, 10000, 50, 10000, 10000},
-//                {10000, 10000, 10000, 10000, 10000, 10},
-//                {10000, 10000, 10000, 20, 10000, 60},
-//                {10000, 10000, 10000, 10000, 10000, 10000},
-//        };
+        String[] vertexes = {"V0", "V1", "V2", "V3", "V4", "V5"};
+        int[][] matrix = new int[][]{
+                {10000, 10000, 10, 10000, 30, 100},
+                {10000, 10000, 5, 10000, 10000, 10000},
+                {10000, 10000, 10000, 50, 10000, 10000},
+                {10000, 10000, 10000, 10000, 10000, 10},
+                {10000, 10000, 10000, 20, 10000, 60},
+                {10000, 10000, 10000, 10000, 10000, 10000},
+        };
         graph.matrix = matrix;
         graph.numVertexes = vertexes.length;
         graph.vertexes = vertexes;
