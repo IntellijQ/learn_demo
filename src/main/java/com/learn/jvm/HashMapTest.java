0a4a19c76d63bce985dff4b0d65904ec3cbfc62e
yangdongsheng2 <yangdongsheng2@9fbank.com.cn>
2020-12-15 17:46:59 +0800

    java基础知识---hashmap

--- src/main/java/com/learn/jvm/HashMapTest.java
+++ src/main/java/com/learn/jvm/HashMapTest.java
@@ -11,19 +11,39 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class HashMapTest {
     public static void main(String[] args) {
-        int number = 10;
+
+        Integer ii = 1212;
+        Integer ii2 = 1212;
+        System.out.println(ii == ii2);
+        System.out.println(ii.equals(ii2));
+
+
+        System.out.println(15&0);
+        //左移一位
+        int test = 10;
+        System.out.println("原始数二进制=" + Integer.toBinaryString(test));
+        test = test >> 2;
+        System.out.println(">>2为" + test + "---" + Integer.toBinaryString(test));
+        test = 3;
+        test = test >>> 2;
+        System.out.println(">>>2为" + test + "---" +  Integer.toBinaryString(test));
+
+
+        int number = 3;
         //原始数二进制
         System.out.println("原始数二进制=" + Integer.toBinaryString(number));
 
         number = number << 1;
+        System.out.println(number);
         //左移一位
+        System.out.println("<< 1=" + Integer.toBinaryString(number));
+        number = number >>> 1;
         System.out.println("<< 1=" + Integer.toBinaryString(number));
 
         number = number >> 1;
         //右移一位
         System.out.println(">> 1=" + Integer.toBinaryString(number));
 
-        number = number >>> 1;
         //右移一位
         int i = "fsfsfdsfsfsdfsdfdsfdsfsd".hashCode();
         System.out.println("key =" + Integer.toBinaryString(i));
@@ -34,21 +54,21 @@ public class HashMapTest {
 
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>(32);
         System.out.println(objectObjectHashMap.toString());
-        objectObjectHashMap.put("dddd","ddd");
+        objectObjectHashMap.put("dddd", "ddd");
         objectObjectHashMap.get("dddd");
-        int n = 18 - 1;
-        n |= n >>> 1;
-        System.out.println(n);
-        n |= n >>> 2;
-        System.out.println(n);
-        n |= n >>> 4;
-        System.out.println(n);
-        n |= n >>> 8;
-        System.out.println(n);
-        n |= n >>> 16;
+        int n2 = 18 - 1;
+        n2 |= n2 >>> 1;
+        System.out.println(n2);
+        n2 |= n2 >>> 2;
+        System.out.println(n2);
+        n2 |= n2 >>> 4;
+        System.out.println(n2);
+        n2 |= n2 >>> 8;
+        System.out.println(n2);
+        n2 |= n2 >>> 16;
 
 //        System.out.println(Math.sqrt((double) n));
-        System.out.println("-----:" + (n+1));
+        System.out.println("-----:" + (n2 + 1));
 
 //
 //        10101000101100101100110001011011
@@ -56,11 +76,11 @@ public class HashMapTest {
 //        10101000101100100110010011101001
 
         ConcurrentHashMap<Object, Object> objectObjectConcurrentHashMap = new ConcurrentHashMap<>();
-        objectObjectConcurrentHashMap.put("dd","ddd");
+        objectObjectConcurrentHashMap.put("dd", "ddd");
     }
 
 
-    class Person{
+    class Person {
 
         @Override
         public int hashCode() {
--- src/main/java/com/learn/jvm/StringTest.java
+++ src/main/java/com/learn/jvm/StringTest.java
@@ -8,14 +8,29 @@ package com.learn.jvm;
  */
 public class StringTest {
     public static void main(String[] args) {
-        String ddd = "d";
-        String dd = new String("d");
-//        System.out.println(dd + "dd");
+//        String str1 = new StringBuilder("计算机").append("软件").toString();
+//        //sun.misc.Version
+//        String str2 = new StringBuilder("Ja").append("va").toString();
 //
-//        String  ddd = "ddd";
-//        System.out.println("ddd" == (dd + "dd"));//flase
-//        System.out.println("ddd" == ddd);//true
+//        System.out.println(str1.intern() == str1);//因为之前没有所以创建的引用和intern()返回的引用相同
+//        System.out.println(str2.intern() == str2);//"java在StringBuilder()之前已经出现过",所以intern()返回的引用与新创建的引用不是同一个
+//
+//        String str3 = "java";
+//        String str4 = new String("java");
+//        System.out.println(str3 == str4);
 
+        String tt1 = "天下第一";
+        String tt2 = new String("天下第一");
+        String tt3 = tt2.intern();
+        System.out.println(tt1 == tt2);//false "天下第一"存在常量池中，归输入堆，tt2指向new String()的一个引用，故不相等
+        System.out.println(tt2 == tt3);//false tt3 会首先检查常量池中，是否有相等字符常的常量，如果有，则指向该对象 （1.6以后）
+        System.out.println(tt1 == tt3);//true
 
+//        System.out.println(ClassLayout.parseInstance(d).toPrintable());
+//        System.out.println(ClassLayout.parseInstance(d).toPrintable());
+
+//        System.out.println(String.format("sizeof(new A):%s", ClassLayout.parseInstance(d).instanceSize()));
+
+//        System.out.println(GraphLayout.parseInstance(d).toPrintable());
     }
 }
